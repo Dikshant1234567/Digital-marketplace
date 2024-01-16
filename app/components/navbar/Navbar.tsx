79d1c68fa43d55d26332221755be6b87fd581df0
Dikshant1234567 <119150042+Dikshant1234567@users.noreply.github.com>
2024-01-16 20:13:39 +0530

    adding mantine

---
+++ app/components/navbar/Navbar.tsx
@@ -0,0 +1,7 @@
+import { Box } from "@mantine/core";
+
+function Navbar() {
+  return <Box>this is box</Box>;
+}
+
+export default Navbar;
---
+++ app/pages/_app.tsx
@@ -0,0 +1,18 @@
+// Import styles of packages that you've installed.
+// All packages except `@mantine/hooks` require styles imports
+import '@mantine/core/styles.css';
+
+import type { AppProps } from 'next/app';
+import { createTheme, MantineProvider } from '@mantine/core';
+
+const theme = createTheme({
+  /** Put your mantine theme override here */
+});
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <MantineProvider theme={theme}>
+      <Component {...pageProps} />
+    </MantineProvider>
+  );
+}
