78a29fd0390ba971147148e31ccfe93b733aa3ee
sagar <sagar.kumar@hpe.com>
2024-01-20 15:02:51 +0530

    Backend setup

---
+++ server/.prettierrc.mjs
@@ -0,0 +1,11 @@
+module.exports = {
+  arrowParens: "always",
+  bracketSpacing: true,
+  bracketSameLine: false,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: "es5",
+  useTabs: false,
+  endOfLine: "lf",
+};
