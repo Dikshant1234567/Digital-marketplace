d6dd87ec409272e00e13ff2cabdcc3e1c59d14be
sagar <sagar.kumar@hpe.com>
2024-01-27 13:33:32 +0530

    Axios for create product

--- app/components/sellerProduct/CreateProductPage.tsx
+++ app/components/sellerProduct/CreateProductPage.tsx
@@ -11,10 +11,19 @@ import {
   Group,
   Text,
 } from "@mantine/core";
-import { useForm } from "@mantine/form";
-import { IconAt } from "@tabler/icons-react";
+import {useForm} from "@mantine/form";
+import {IconAt} from "@tabler/icons-react";
+import axios from "axios";
 import Link from "next/link";
-import React, { useState } from "react";
+import React, {useState} from "react";
+
+export interface productType {
+  productDescription: String;
+  productName: String;
+  productImage: File | null;
+  price: Number | string;
+  category: String;
+}
 
 function CreateProductPage() {
   const [image, setImage] = useState<File | null>(null);
@@ -27,27 +36,28 @@ function CreateProductPage() {
       productImage: image,
     },
   });
-  // console.log(typeof form.values.price, form.values.price);
 
-  let { productDescription, productImage, productName, price, category } =
-    form.values;
-    console.log(productDescription, productImage, productName, price, category,'pppppppp')
+  function handleForm(values: productType) {
+    const form_Data = new FormData();
+    // @ts-ignore
+    Object.keys(values).forEach((key) => form_Data.append(key, values[key]));
 
-  console.log(price);
+    axios.post("http://localhost:5050/product/create", form_Data).then((e) => {
+      console.log(e);
+    });
+  }
+
   return (
     <Box>
       <Group my={10} align="baseline">
         <Title fz={35} fw={"600"}>
-          <Link
-            href={"/myProduct"}
-            style={{ textDecoration: "none", color: "black" }}
-          >
+          <Link href={"/myProduct"} style={{textDecoration: "none", color: "black"}}>
             Myproduct
           </Link>
         </Title>
         <Text fz={30}>\</Text>
         <Title fz={25} fw={"lighter"}>
-          <Link href={"/"} style={{ textDecoration: "none", color: "black" }}>
+          <Link href={"/"} style={{textDecoration: "none", color: "black"}}>
             Order
           </Link>
         </Title>
@@ -60,35 +70,35 @@ function CreateProductPage() {
           height: "4rem",
           textTransform: "capitalize",
           background: "default",
-        }}
-      >
-        {productName}
-      </Title>
+        }}></Title>
       {/* Product details form */}
       <Box>
-        <form onSubmit={form.onSubmit((values) => console.log(values,'valuesss'))}>
+        <form
+          onSubmit={form.onSubmit((values) => {
+            handleForm(values);
+          })}>
           <TextInput
             label="Name"
             placeholder="Product Name"
             required
             my={12}
             value={form.values.productName}
-            {...form.getInputProps('productName')}
+            {...form.getInputProps("productName")}
           />
           <Textarea
             label="Product Description"
             required
             placeholder="Enter description"
             value={form.values.productDescription}
-            {...form.getInputProps('productDescription')}
+            {...form.getInputProps("productDescription")}
           />
           <NumberInput
             label="Price in USD"
             required
             my={12}
-            value={form.values.price}
+            value={Number(form.values.price)}
             placeholder="Enter price"
-            {...form.getInputProps('price')}
+            {...form.getInputProps("price")}
           />
           <Select
             label="Catogry"
@@ -96,14 +106,14 @@ function CreateProductPage() {
             data={["React", "Angular", "Vue", "Svelte"]}
             value={form.values.category}
             required
-            {...form.getInputProps('category')}
+            {...form.getInputProps("category")}
           />
           <FileInput
             label="Select file"
             my={12}
             value={form.values.productImage}
             placeholder="Upload image"
-            {...form.getInputProps('productImage')}
+            {...form.getInputProps("productImage")}
           />
           <Button type="submit" variant="gradient" mr={"xs"} mt={"sm"}>
             Register
